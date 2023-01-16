039d5e78ad3e872492530a7edcdeb53c4b91b1d8
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-16 17:24:08 +0530

    Phone number added

---
+++ UserDetails.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{ 
+    public class UserDetails
+    {
+        public static string Reg_pattern1 = "^[A-Z]{1}[a-z]{2}[a-z]+$";
+        public static string Reg_pattern2 = "^[A-Z]{1}[a-z]{2}[a-z]+$";
+        public static string Reg_pattern3 = "^[a-zA-Z]+[@]{1}[a-zA-Z]+[.]{1}[a-z]{3}$";
+        public static string Reg_pattern4 = "^[0-9]{10}$";
+        public void UserDetail()
+        {
+            Console.WriteLine("Enter your First name : ");
+            string fname = Console.ReadLine(); 
+            Console.WriteLine("Enter your Last name : ");
+            string lname = Console.ReadLine();
+            Console.WriteLine("Enter your Email : ");
+            string em = Console.ReadLine();
+            Console.WriteLine("Enter your Phone number : ");
+            string ph = Console.ReadLine();
+
+            bool val1 = Regex.IsMatch(fname, Reg_pattern1);
+            bool val2 = Regex.IsMatch(lname, Reg_pattern2);
+            bool val3 = Regex.IsMatch(em, Reg_pattern3);
+            bool val4 = Regex.IsMatch(ph, Reg_pattern4);
+
+            if (!val1)
+              Console.WriteLine("Invalid First name");
+            if(!val2)
+              Console.WriteLine("Invalid Last name");
+            if (!val3)
+                Console.WriteLine("Invalid Email");
+            if (!val4)
+                Console.WriteLine("Invalid PhoneNumber");
+
+        }
+    }
+}
