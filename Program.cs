ffe334e05dc3426b65ce76f1cb0207343b7bcf9f
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-16 17:11:04 +0530

    First name added

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace UserRegistration
         
         public static void Main(String[] args)
         {
-            Console.WriteLine("Welcome");
+            Console.WriteLine("User Registration");
+            UserDetails user = new UserDetails();
+            user.UserDetail();
 
         }
     }
