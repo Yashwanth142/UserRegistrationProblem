9c9a6cfb24137aa269bdeb3b5c5928f489c8a9f8
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-24 17:50:44 +0530

    try catch added

--- Program.cs
+++ Program.cs
@@ -13,9 +13,26 @@ namespace UserRegistration
             Console.WriteLine("enter your Last  name");
             string lname = Console.ReadLine();
             Console.WriteLine();
-            Console.WriteLine("enter your mail");
-            string mail = Console.ReadLine();
+
+            Console.WriteLine("enter HOW many emails u want to validate");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("enter your mail");
+                string mail = Console.ReadLine();
+                demo d1 = new demo();
+                try
+                {
+                    d1.validmail(mail);
+                }
+                catch (InvalidException e)
+                {
+                    Console.WriteLine("Oops {0} ! ", e.Message);
+                }
+            }
             Console.WriteLine();
+
+
             Console.WriteLine("enter your mobile number");
             string mb = Console.ReadLine();
             Console.WriteLine();
@@ -23,20 +40,66 @@ namespace UserRegistration
             string pwd = Console.ReadLine();
             Console.WriteLine();
 
+            demo d = new demo();
+            try
+            {
+                d.validname(name);
+            }
+            catch (InvalidException e)
+            {
+                Console.WriteLine("Oops {0} ! ", e.Message);
+            }
+
+            try
+            {
+                d.validname(lname);
+            }
+            catch (InvalidException e)
+            {
+                Console.WriteLine("Oops {0} ! ", e.Message);
+            }
+
+            try
+            {
+                d.validname(mb);
+            }
+            catch (InvalidException e)
+            {
+                Console.WriteLine("Oops {0} ! ", e.Message);
+            }
+
+            try
+            {
+                d.validname(pwd);
+            }
+            catch (InvalidException e)
+            {
+                Console.WriteLine("Oops {0} ! ", e.Message);
+            }
+
         }
     }
     public class demo
     {
-        public static string Reg_pattern1 = "^[A-Z]{1}[A-Za-z]{2,}$";
-        public static string Reg_pattern2 = "^[A-Z]{1}[A-Za-z]{2,}$";
-        public static string Reg_pattern3 = "^[A-Za-z0-9]+([.+-][A-Za-z0-9]+)*@[A-Za-z0-9]+[.][a-z]{2,}([.][a-z]{2,})?$";
-        public static string Reg_pattern4 = "^[0-9]{2}\\s[0-9]{10}$";
+    public Regex Reg_pattern1 = new Regex(@"^[A-Z]{1}[A-Za-z]{2,}$");
+    public Regex Reg_pattern2 = new Regex(@"^[A-Z]{1}[A-Za-z]{2,}$");
+    public Regex Reg_pattern3 = new Regex(@"^[A-Za-z0-9]+([.+-][A-Za-z0-9]+)*@[A-Za-z0-9]+[.][a-z]{2,}([.][a-z]{2,})?$");
+    public Regex Reg_pattern4 = new Regex(@"^[0-9]{2}\s[0-9]{10}$");
+    public Regex Reg_pattern5 = new Regex(@"^.*(?=.{8,})(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).*$");
+    public Regex Reg_pattern5_1 = new Regex(@"^[a-zA-Z]{8,}([0-9]+)?$");
+    public Regex Reg_pattern5_2 = new Regex(@"^[A-Z]{1,}[a-zA-Z]{7,}([0-9]+)?$");
+    public Regex Reg_pattern5_3 = new Regex(@"^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}([0-9]+)?$");
 
-        public static string Reg_pattern5 = "^.*(?=.{8,})(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).*$";
-        public static string Reg_pattern5_1 = "^[a-zA-Z]{8,}([0-9]+)?$";
-        public static string Reg_pattern5_2 = "^[A-Z]{1,}[a-zA-Z]{7,}([0-9]+)?$";
-        public static string Reg_pattern5_3 = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}([0-9]+)?$";
-        public bool validname(string name)
+    //public static string Reg_pattern1 = "^[A-Z]{1}[A-Za-z]{2,}$";
+    //public static string Reg_pattern2 = "^[A-Z]{1}[A-Za-z]{2,}$";
+    //public static string Reg_pattern3 = "^[A-Za-z0-9]+([.+-][A-Za-z0-9]+)*@[A-Za-z0-9]+[.][a-z]{2,}([.][a-z]{2,})?$";
+    //public static string Reg_pattern4 = "^[0-9]{2}\\s[0-9]{10}$";
+
+    //public static string Reg_pattern5 = "^.*(?=.{8,})(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).*$";
+    //public static string Reg_pattern5_1 = "^[a-zA-Z]{8,}([0-9]+)?$";
+    //public static string Reg_pattern5_2 = "^[A-Z]{1,}[a-zA-Z]{7,}([0-9]+)?$";
+    //public static string Reg_pattern5_3 = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}([0-9]+)?$";
+    public bool validname(string name)
         {
             bool val1 = Regex.IsMatch(name, Reg_pattern1);
             if (val1)
